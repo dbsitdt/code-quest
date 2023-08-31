0aeecd47b4a926e57e5236949edbd413a440bca3
WarriorX623 <owenhwtam@gmail.com>
2023-08-31 14:52:17 +0800

    style error 404 page

--- error.vue
+++ error.vue
@@ -1,11 +1,42 @@
 <template>
-  <NuxtLayout>
-    <div>
-      <h3>{{ error.statusCode }}</h3>
-      <p>Hi</p>
-    </div>
-  </NuxtLayout>
+  <div class="error-container">
+    <h4>{{ error.statusCode }}</h4>
+    <h3>Oops! Something went wrong!</h3>
+    <p>{{ error.message }}</p>
+    <NuxtLink to="/">Return to Home</NuxtLink>
+  </div>
 </template>
 <script setup>
 defineProps(["error"]);
 </script>
+<style scoped>
+.error-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+}
+a {
+  padding: 0.5rem 1.2rem;
+  background: #278bff;
+  color: white;
+  font-size: 1rem;
+  border-radius: 50px;
+  border: none;
+  -webkit-border: none;
+  font-family: inherit;
+  cursor: pointer;
+  font-family: Roboto Mono;
+  margin-top: 1rem;
+  text-decoration: none;
+}
+h3 {
+  font-size: clamp(2rem, 4vw, 4.5rem);
+}
+h4 {
+  font-size: clamp(1rem, 3vw, 3rem);
+}
+</style>
