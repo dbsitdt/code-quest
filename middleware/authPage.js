be6288b6e65ab19d138e392bbb6410968d0adfda
WarriorX623 <owenhwtam@gmail.com>
2023-09-04 17:11:07 +0800

    Fix routing bug

--- middleware/authPage.js
+++ middleware/authPage.js
@@ -1,12 +1,11 @@
 import { useAuthStore } from "../stores/auth.ts";
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const store = useAuthStore();
-  if (from.path == "/login") {
+  if (from.path === "/login" || from.path === to.path) {
     if (store.isTokenExpired) {
       store.logout();
       return navigateTo("/login", { redirectCode: 301 });
     }
-
     if (!store.authStatus) {
       return navigateTo("/login", { redirectCode: 301 });
     }
