6ffff3681d9eea9cbbb3ea96859888af6c9c27ff
WarriorX623 <owenhwtam@gmail.com>
2024-01-30 00:49:27 +0800

    Added security package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,7 +29,42 @@ export default defineNuxtConfig({
       ],
     },
   },
-  modules: ["@pinia/nuxt"],
+  modules: ["nuxt-security", "@pinia/nuxt"],
+  security: {
+    headers: {
+      contentSecurityPolicy: {
+        "script-src": [
+          "'self'", // Fallback value, will be ignored by most modern browsers (level 3)
+          "https:", // Fallback value, will be ignored by most modern browsers (level 3)
+          "'unsafe-inline'", // Fallback value, will be ignored by almost any browser (level 2)
+          "'strict-dynamic'", // Strict CSP via 'strict-dynamic', supported by most modern browsers (level 3)
+          "'nonce-{{nonce}}'", // Enables CSP nonce support for scripts in SSR mode, supported by almost any browser (level 2)
+        ],
+        "style-src": [
+          "'self'", // Enables loading of stylesheets hosted on same origin
+          "https:", // For increased security, replace by the specific hosting domain or file name of your external stylesheets
+          "'unsafe-inline'", // Recommended default for most Nuxt apps
+        ],
+        "img-src": ["'self'", "data:"], // Add relevant https://... sources if you load images from external sources
+        "font-src": ["'self'", "https:", "data:"], //  For increased security, replace by the specific sources for fonts
+        "base-uri": ["'none'"],
+        "object-src": ["'none'"],
+        "script-src-attr": ["'none'"],
+        "form-action": ["'self'"],
+        "frame-ancestors": ["'self'"],
+        "upgrade-insecure-requests": true,
+      },
+    },
+    sri: true,
+    rateLimiter: {
+      tokensPerInterval: 100,
+      interval: 60 * 60 * 1000,
+      headers: true,
+    },
+    requestSizeLimiter: {
+      maxRequestSizeInBytes: 10000,
+    },
+  },
   pinia: {
     autoImports: [
       // automatically imports `defineStore`
